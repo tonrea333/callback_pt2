261eb98764a74a3b4decf50a21f34d7c242e64f8
ginnygu <ginnygu@gmail.com>
2021-09-09 11:08:14 -0400

    initial commit

---
+++ functions.js
@@ -0,0 +1,53 @@
+const indexOf = function (array, target) {
+  //array = [1], target = 1
+  var result = -1;
+
+  each(array, function (item, index) {
+    //item = 1
+    if (item === target && result === -1) {
+      result = index;
+      //result = 1
+    }
+  });
+
+  return result;
+};
+
+const each = function (collection, iterator) {
+  if (collection instanceof Array) {
+    for (let i = 0; i < collection.length; i++) {
+      iterator(collection[i], i, collection);
+    }
+  } else if (collection instanceof Object) {
+    for (let prop in collection) {
+      iterator(collection[prop], prop, collection);
+    }
+  }
+};
+
+const map = function (collection, iterator) {
+  var result = [];
+
+  each(collection, function (element, index, array) {
+    result.push(iterator(element));
+  });
+
+  return result;
+};
+
+const filter = function (collection, callback) {};
+//reject([1, 2, 3, 4, 5, 6], isEven);
+const reject = function (collection, callbackTest) {};
+
+//use indexOf to solve this
+const uniq = function (array) {};
+
+const reduce = function (collection, iterator, accumulator) {};
+
+module.exports = {
+  filter,
+  reject,
+  uniq,
+  reduce,
+  map,
+};
