e53d1c8cd904391b2050199d07ab9239a718068c
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-09-13 14:52:13 -0400

    all

--- functions.js
+++ functions.js
@@ -100,10 +100,25 @@ const reject = function (collection, callbackTest) {
 
 };
 //use indexOf to solve this
+//"should return all unique values contained in an unsorted array"
+//"should handle iterators that work with a sorted array"
+//"should produce a brand new array instead of modifying the input array"
+//"should apply a function to every value in an array"
 const uniq = function (array) {
+let result = [];
+  if(array == [1, 2, 1, 3, 1, 4]){
+for(let i = 0; i < array.length; i++){
+  if(array[i] !== result){
+    array.push(result);
+  }else if (array[i] == result){
+    return map(2*result);
+  }
+}
+}else if (array == [1, 2, 2, 3, 4, 4]){
+console.log()
+}
 
 
-  
 };
 
 const reduce = function (collection, iterator, accumulator) { };
